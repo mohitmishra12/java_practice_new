5a22b4fcad10826abeafe20442a6760092e5c4fa
mohitmishra12 <mohitmishra747041@gmail.com>
2026-01-27 19:42:29 +0530

    looping for practice

---
+++ java/src/loops/forLoops.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class forLoops {
+    public static void main(String[] args) {
+
+        int arr = 30;
+
+        for (int i = 0; i <= arr; i++) {
+            if(arr == 12){
+                System.out.println("value are equal to 2");
+            }else {
+                System.out.println("countinue....");
+            }
+//            System.out.println(i);
+
+        }
+    }
+}
