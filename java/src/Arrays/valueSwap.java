12b454d9de11aae331f9a13338ab4cd31312e69e
mohitmishra12 <mohitmishra747041@gmail.com>
2026-02-04 18:47:52 +0530

    swap int value

---
+++ java/src/Arrays/valueSwap.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class valueSwap {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+//        a=10 and b =20
+
+        a = a +b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("a =" + a);
+        System.out.println("b =" + b);
+
+
+
+    }
+}
