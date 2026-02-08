bc438de9e6aba5ec780f2a13971adc09cd67bc17
mohitmishra12 <mohitmishra747041@gmail.com>
2026-02-08 18:02:56 +0530

    User input with scanner

---
+++ java/src/Math/minOperation.java
@@ -0,0 +1,11 @@
+package Math;
+
+public class minOperation {
+    public static void main(String[] args) {
+
+
+
+        System.out.println(Math.sqrt(81));
+        System.out.println(Math.sqrt(16));
+    }
+}
