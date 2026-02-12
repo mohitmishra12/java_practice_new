d8fe797732a0ec016c5f5cc84d9e994341042308
mohitmishra12 <mohitmishra747041@gmail.com>
2026-02-12 14:26:38 +0530

    threading practice

---
+++ java/src/String/throwExp.java
@@ -0,0 +1,16 @@
+package String;
+
+class TestThrow {
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Not eligible to vote");
+        }
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        checkAge(20);
+    }
+}
+
