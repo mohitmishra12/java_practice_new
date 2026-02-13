81a416d7dd2968d833ccbc6dcd0407b04e52e10e
mohitmishra12 <mohitmishra747041@gmail.com>
2026-02-13 18:24:44 +0530

    threading practice

--- java/src/String/Throwss.java
+++ java/src/String/Throwss.java
@@ -1,29 +1,29 @@
-package String;
-
-
-import java.io.FileReader;
-
-class TestThrows{
-    static void  readFile() throws ArithmeticException{
-
-        FileReader file = new FileReader("abc.txt");
-
-    }
-
-}
-
-
-
-public class Throwss {
-    public static void main(String[] args) {
-        try {
-            TestThrows.readFile();
-        }
-        catch (Exception e){
-            System.out.println();
-        }
-
-
-    }
-}
-
+//package String;
+//
+//
+//import java.io.FileReader;
+//
+//class TestThrows{
+//    static void  readFile() throws ArithmeticException{
+//
+//        FileReader file = new FileReader("abc.txt");
+//
+//    }
+//
+//}
+//
+//
+//
+//public class Throwss {
+//    public static void main(String[] args) {
+//        try {
+//            TestThrows.readFile();
+//        }
+//        catch (Exception e){
+//            System.out.println();
+//        }
+//
+//
+//    }
+//}
+//
