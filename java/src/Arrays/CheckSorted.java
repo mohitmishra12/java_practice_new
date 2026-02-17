8b10ce174b564d1f64dec171a5255076a3cebfd7
mohitmishra12 <mohitmishra747041@gmail.com>
2026-02-17 18:50:12 +0530

    check sort and Collection with array and bitwise

--- java/src/Arrays/CheckSorted.java
+++ java/src/Arrays/CheckSorted.java
@@ -3,14 +3,8 @@ package Arrays;
 public class CheckSorted {
     public static void main(String[] args) {
 
-
         int[] arr = {1, 2, 3, 4, 5};
-
         boolean isSorted = true;
-
-
-
-
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[i - 1]) {
