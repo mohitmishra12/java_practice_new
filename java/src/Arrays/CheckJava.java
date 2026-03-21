dcd609989b57e2b09d7bd7ace958b4d2b740fd6c
mohitmishra12 <mohitmishra747041@gmail.com>
2026-03-21 21:54:21 +0530

    new changes

---
+++ java/src/Arrays/CheckJava.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+ class mynewClass{
+     int num = 12;
+     String str = "Rahul";
+
+}
+public class CheckJava {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("yes"));
+    }
+}
