c077053549d98d479f62d5ef16452a2478633d06
mohitmishra12 <mohitmishra747041@gmail.com>
2026-03-27 11:46:30 +0530

    new changes in binary matrix

--- java/src/Arrays/BinaryMatrix.java
+++ java/src/Arrays/BinaryMatrix.java
@@ -5,7 +5,9 @@ class BinaryMatrix {
         int m = mat.length, n = mat[0].length;
         int[] r = new int[m];
         int[] c = new int[n];
+
         for (int i = 0; i < m; ++i) {
+
             for (int j = 0; j < n; ++j) {
                 r[i] += mat[i][j];
                 c[j] += mat[i][j];
