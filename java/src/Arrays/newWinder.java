be52a659f0ee3d2ece466c5ef6069f9d9ee5b772
mohitmishra12 <mohitmishra747041@gmail.com>
2026-06-08 18:23:14 +0530

    leed code practice Question

--- java/src/Arrays/newWinder.java
+++ java/src/Arrays/newWinder.java
@@ -11,10 +11,12 @@ class newWinder {
     private int calc(int[] a1, int[] t1, int[] a2, int[] t2) {
         int minEnd = Integer.MAX_VALUE;
         for (int i = 0; i < a1.length; ++i) {
+
             minEnd = Math.min(minEnd, a1[i] + t1[i]);
         }
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < a2.length; ++i) {
+
             ans = Math.min(ans, Math.max(minEnd, a2[i]) + t2[i]);
         }
         return ans;
